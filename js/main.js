064645f5264b124c840d6d642bdd4b824f32f156
jskube1 <60903225+jskube1@users.noreply.github.com>
2021-04-29 13:10:44 -0400

    added map tags

--- js/main.js
+++ js/main.js
@@ -5,3 +5,16 @@ $(document).ready(function() {
 console.log('hello');
 
 });
+
+$( ".close-icon" ).click(function() {
+  $( this ).parent().fadeToggle( "slow", "linear" );
+  event.preventDefault();
+  event.stopPropagation();
+});
+
+$( ".bird" ).click(function() {
+  $( ".bird-overlay" ).fadeToggle( "slow", "linear" );
+  event.preventDefault();
+  event.stopPropagation();
+});
+
